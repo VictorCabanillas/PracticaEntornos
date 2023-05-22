3a04b8253193b57c335a5da03f64868116538836
VictorCabanillas <vector.uno4@gmail.com>
2023-05-22 14:13:28 +0200

    PlayerHealth y modificaciones a weapon
    Añadida clase player health y modificado el impacto del arma a otro jugador para mejorarlo y añadir la reduccion de vida

---
+++ EM-practica-2022-2023/Assets/Scripts/Fighting/PlayerHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerHealth : NetworkBehaviour
+{
+    NetworkVariable<int> Health;
+    public GameObject healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health.Value = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DecreaseHealth(int amount) 
+    {
+        Health.Value -= amount;
+    }
+}
