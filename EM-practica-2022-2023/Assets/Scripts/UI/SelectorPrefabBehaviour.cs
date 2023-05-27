49db4c07706fa129dde56dc3047119a2ca1e34f1
Aisayan <aisayan68@gmail.com>
2023-05-27 23:25:12 +0200

    Avance en el selector de personajes, datos compartidos de los personajes, y menú principal

---
+++ EM-practica-2022-2023/Assets/Scripts/UI/SelectorPrefabBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UI;
+using TMPro;
+
+public class SelectorPrefabBehaviour : NetworkBehaviour
+{
+    UiManager uiManager;
+
+    public override void OnNetworkSpawn()
+    {
+        GameObject barra = uiManager.CrearBarras((int)OwnerClientId);
+        var playerLobby = barra.GetComponent<PlayerLobby>();
+        var playerData = GetPlayerData();
+        playerLobby.SetData(playerData.Name.Value, "Eligiendo", null);
+                  
+    }
+
+    PlayerData GetPlayerData()
+    {
+        return FindObjectOfType<PlayerData>();
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        uiManager = GameObject.FindGameObjectWithTag("Canvas").GetComponent<UiManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- EM-practica-2022-2023/Assets/Scripts/UI/UIHandler.cs
+++ EM-practica-2022-2023/Assets/Scripts/UI/UIHandler.cs
@@ -1,6 +1,9 @@
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.SceneManagement;
 
 namespace UI
 {
@@ -9,23 +12,107 @@ namespace UI
         public GameObject debugPanel;
         public Button hostButton;
         public Button clientButton;
+        public Button serverButton;
+        public Button confirmNameButton;
+        private bool hostOrClient = false; //Booleano para determinar que acción se va a ejecutar tras la elección del nombre en función de si hemos elegido host o client
+        public TextMeshProUGUI namePlayer;
+        public TextMeshProUGUI IP;
+        public TextMeshProUGUI Port;
+
+        UnityTransport unityTransport; // Cambiar ip y puerto en función del introducido
+
+        [SerializeField] GameObject nameSelectorPanel;
 
         private void Start()
         {
             hostButton.onClick.AddListener(OnHostButtonClicked);
             clientButton.onClick.AddListener(OnClientButtonClicked);
+            serverButton.onClick.AddListener(OnServerButtonClicked);
+            confirmNameButton.onClick.AddListener(ConfirmNameButtonClicked);
+
+            unityTransport = NetworkManager.Singleton.gameObject.GetComponent<UnityTransport>();
         }
 
+        //Host: Se ocultan los botones y se muestra el selector de nombre, además, se pone a true el booleano que en el metodo ConfirmNameButton() nos va a hacer el Start Host
         private void OnHostButtonClicked()
         {
-            NetworkManager.Singleton.StartHost();
+            string ip = IP.text;
+            string port = Port.text;
+
+            port = port.Remove(port.Length - 1, 1); // Para quitar el extra character (hueco vacio adicional) que crea el string y poder parsearlo bien
+
+            unityTransport.ConnectionData.Address = ip;
+            unityTransport.ConnectionData.Port = ushort.Parse(port);
+
+            hostOrClient = true;
+            Hide();
+            ShowNameSelector();
+        }
+        //Client: Se ocultan los botones y se muestra el selector de nombre, además, se pone a false el booleano que en el metodo ConfirmNameButton() nos va a hacer el Start Client
+        private void OnClientButtonClicked()
+        {
+            string ip = IP.text;
+            string port = Port.text;
+
+            port = port.Remove(port.Length - 1, 1); // Para quitar el extra character (hueco vacio adicional) que crea el string y poder parsearlo bien
+
+            unityTransport.ConnectionData.Address = ip;
+            unityTransport.ConnectionData.Port = ushort.Parse(port);
+
+            hostOrClient = true;
+            Hide();
+            ShowNameSelector();
+        }
+
+        private void OnServerButtonClicked()
+        {
+            string ip = IP.text;
+            string port = Port.text;
+
+
+            port  = port.Remove(port.Length-1, 1); // Para quitar el extra character (hueco vacio adicional) que crea el string y poder parsearlo bien
+
+            unityTransport.ConnectionData.Address = ip;
+            unityTransport.ConnectionData.Port = ushort.Parse(port);
+
+
+            NetworkManager.Singleton.StartServer();
+            Hide();
+        }
+
+        //Para esconder el botón
+        public void Hide()
+        {
             debugPanel.SetActive(false);
         }
 
-        private void OnClientButtonClicked()
+        //Para mostrar el botón
+        public void ShowNameSelector()
         {
-            NetworkManager.Singleton.StartClient();
-            debugPanel.SetActive(false);
+            nameSelectorPanel.SetActive(true);
         }
+
+        //Relacionado con el botón de confirmación a la hora de escribir el nombre en la casilla correspondiente
+        public void ConfirmNameButtonClicked()
+        {
+            string nombre = namePlayer.text;
+
+            PlayerPrefs.SetString("NombreJugador", nombre); //En las player preferences va a haber un apartado NombreJugador que se va a pasar entre escenas   
+
+            if (hostOrClient)
+            {
+                
+                NetworkManager.Singleton.StartHost(); 
+            }
+            else
+            {
+                
+                NetworkManager.Singleton.StartClient();
+            }
+
+            SceneManager.LoadScene("CharacterSelector");
+
+        }
+
     }
 }
