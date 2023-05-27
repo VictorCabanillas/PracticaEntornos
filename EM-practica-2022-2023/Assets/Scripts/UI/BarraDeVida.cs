bcd6d25e94b0c512968f075644370a6917ef572b
bugagus <101557679+bugagus@users.noreply.github.com>
2023-05-27 19:31:52 +0200

    cambios grandes

--- EM-practica-2022-2023/Assets/Scripts/UI/BarraDeVida.cs
+++ EM-practica-2022-2023/Assets/Scripts/UI/BarraDeVida.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using TMPro;
 
 public class BarraDeVida : MonoBehaviour
 {
-    [SerializeField] private float vidaMaxima;
+    
+     [SerializeField] private float vidaMaxima;
+     [SerializeField] GameObject relleno;
+     [SerializeField] GameObject icono;
+     [SerializeField] GameObject nombre;
 
     public void CambiarBarra(int current)
     {
         //vidaActual = player.GetComponent<>().GetVida();
-        Debug.Log("Cambiar valor barra");
         GetComponent<Image>().fillAmount = current / vidaMaxima;
+        Debug.Log("Cambiar valor barra");
+        relleno.GetComponent<Image>().fillAmount = current / vidaMaxima;
+    }
+
+    public void SetNombre(String n)
+    {
+        nombre.GetComponent<TMP_Text>().text = n;
+    }
+
+    public void SetIcono(Sprite s)
+    {
+        icono.GetComponent<Image>().sprite = s;
     }
 }
+
