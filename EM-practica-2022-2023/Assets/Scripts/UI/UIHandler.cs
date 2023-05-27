7c73015d8db9a0fc6fdfef81f7326632589587d5
Aisayan <aisayan68@gmail.com>
2023-05-27 11:56:45 +0200

    Selector nombre e ip

--- EM-practica-2022-2023/Assets/Scripts/UI/UIHandler.cs
+++ EM-practica-2022-2023/Assets/Scripts/UI/UIHandler.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,23 +10,44 @@ namespace UI
         public GameObject debugPanel;
         public Button hostButton;
         public Button clientButton;
+        public Button serverButton;
+        public TextMeshProUGUI IPText;
 
         private void Start()
         {
             hostButton.onClick.AddListener(OnHostButtonClicked);
             clientButton.onClick.AddListener(OnClientButtonClicked);
+            serverButton.onClick.AddListener(OnServerButtonClicked);
         }
 
         private void OnHostButtonClicked()
         {
             NetworkManager.Singleton.StartHost();
-            debugPanel.SetActive(false);
+            HidePanel();
         }
 
         private void OnClientButtonClicked()
         {
             NetworkManager.Singleton.StartClient();
+            HidePanel();
+        }
+
+        //Método para cuando clicamos el boton de server 
+        private void OnServerButtonClicked()
+        {
+            NetworkManager.Singleton.StartServer();
+            HidePanel();
+        }
+        //Esconder paneles del boton
+        public void HidePanel()
+        {
             debugPanel.SetActive(false);
+        }
+
+        //Mostrar boton
+        public void DisplayPanel()
+        {
+            debugPanel.SetActive(true);
         }
     }
 }
